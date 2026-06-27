aadc2e2acbc9a44bf9b7669ecadba11b926530b5
kashif ibraheem <arsalankashif8@gmail.com>
2026-06-27 09:13:14 +0500

    feat: add PiP mode on home button - LaughCoin

--- app/src/main/java/com/lgcinovations/laughcoin/MainActivity.kt
+++ app/src/main/java/com/lgcinovations/laughcoin/MainActivity.kt
@@ -3,7 +3,11 @@ package com.lgcinovations.laughcoin
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import android.app.PictureInPictureParams
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.util.Rational
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
